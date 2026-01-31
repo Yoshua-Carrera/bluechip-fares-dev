eb2b9f01717214f3775e52cb6e255894903552ee
Yoshua <54082330+Yoshua-Carrera@users.noreply.github.com>
2026-01-30 22:41:19 -0500

    feature: test (#14)
    * feature: test
    
    * testing new embed
    
    * testing embed
    
    * trying with an absolute path
    
    * new try
    
    * raw link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -51,7 +51,8 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       },
       {
         name: 'og:image',
-        content: '/logo/bluechip-fares-logo.jpg',
+        content:
+          'https://raw.githubusercontent.com/Yoshua-Carrera/bluechip-fares-dev/322dece39bbdf5723da33eaece3dc8740583a0e5/public/logo/bluechip-fares-logo.jpg',
       },
       {
         name: 'og:type',
