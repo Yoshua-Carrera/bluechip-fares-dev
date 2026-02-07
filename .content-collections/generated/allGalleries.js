71c566eac5fd0c199c984fa4bcde1225f3c87b6b
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-02-06 18:51:34 -0600

    feature: pretty

--- .content-collections/generated/allGalleries.js
+++ .content-collections/generated/allGalleries.js
@@ -1,199 +1,198 @@
-
 export default [
   {
-    "name": "Bathroom",
-    "location": "Houston, TX",
-    "headshot": "gallery/bathroom.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "bathroom.md",
-      "fileName": "bathroom.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "bathroom"
+    name: 'Bathroom',
+    location: 'Houston, TX',
+    headshot: 'gallery/bathroom.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'bathroom.md',
+      fileName: 'bathroom.md',
+      directory: '.',
+      extension: 'md',
+      path: 'bathroom',
     },
-    "slug": "bathroom"
+    slug: 'bathroom',
   },
   {
-    "name": "Blow in insulation",
-    "location": "Addison, TX",
-    "headshot": "gallery/blow-in-insulation.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "blow-in-insulation.md",
-      "fileName": "blow-in-insulation.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "blow-in-insulation"
+    name: 'Blow in insulation',
+    location: 'Addison, TX',
+    headshot: 'gallery/blow-in-insulation.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'blow-in-insulation.md',
+      fileName: 'blow-in-insulation.md',
+      directory: '.',
+      extension: 'md',
+      path: 'blow-in-insulation',
     },
-    "slug": "blow-in-insulation"
+    slug: 'blow-in-insulation',
   },
   {
-    "name": "New cabinet installation",
-    "location": "Lewisville, TX",
-    "headshot": "gallery/cabinet-installation.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "cabinet-installation.md",
-      "fileName": "cabinet-installation.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "cabinet-installation"
+    name: 'New cabinet installation',
+    location: 'Lewisville, TX',
+    headshot: 'gallery/cabinet-installation.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'cabinet-installation.md',
+      fileName: 'cabinet-installation.md',
+      directory: '.',
+      extension: 'md',
+      path: 'cabinet-installation',
     },
-    "slug": "new-cabinet-installation"
+    slug: 'new-cabinet-installation',
   },
   {
-    "name": "Gym ceiling painting",
-    "location": "Plano, TX",
-    "headshot": "gallery/ceiling-painting.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "ceiling-painting.md",
-      "fileName": "ceiling-painting.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "ceiling-painting"
+    name: 'Gym ceiling painting',
+    location: 'Plano, TX',
+    headshot: 'gallery/ceiling-painting.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'ceiling-painting.md',
+      fileName: 'ceiling-painting.md',
+      directory: '.',
+      extension: 'md',
+      path: 'ceiling-painting',
     },
-    "slug": "gym-ceiling-painting"
+    slug: 'gym-ceiling-painting',
   },
   {
-    "name": "Exterior painting",
-    "location": "San Antonio, TX",
-    "headshot": "gallery/exterior-painting.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "exterior-painting.md",
-      "fileName": "exterior-painting.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "exterior-painting"
+    name: 'Exterior painting',
+    location: 'San Antonio, TX',
+    headshot: 'gallery/exterior-painting.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'exterior-painting.md',
+      fileName: 'exterior-painting.md',
+      directory: '.',
+      extension: 'md',
+      path: 'exterior-painting',
     },
-    "slug": "exterior-painting"
+    slug: 'exterior-painting',
   },
   {
-    "name": "Kitchen remodeling, cabinet installation",
-    "location": "Celina, TX",
-    "headshot": "gallery/kitchen-remodeling.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "kitchen-remodeling.md",
-      "fileName": "kitchen-remodeling.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "kitchen-remodeling"
+    name: 'Kitchen remodeling, cabinet installation',
+    location: 'Celina, TX',
+    headshot: 'gallery/kitchen-remodeling.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'kitchen-remodeling.md',
+      fileName: 'kitchen-remodeling.md',
+      directory: '.',
+      extension: 'md',
+      path: 'kitchen-remodeling',
     },
-    "slug": "kitchen-remodeling-cabinet-installation"
+    slug: 'kitchen-remodeling-cabinet-installation',
   },
   {
-    "name": "Outdoor deck",
-    "location": "Plano, TX",
-    "headshot": "gallery/outdoor-deck.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "outdoor-deck.md",
-      "fileName": "outdoor-deck.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "outdoor-deck"
+    name: 'Outdoor deck',
+    location: 'Plano, TX',
+    headshot: 'gallery/outdoor-deck.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'outdoor-deck.md',
+      fileName: 'outdoor-deck.md',
+      directory: '.',
+      extension: 'md',
+      path: 'outdoor-deck',
     },
-    "slug": "outdoor-deck"
+    slug: 'outdoor-deck',
   },
   {
-    "name": "Outdoor permanent lighting",
-    "location": "Little Elm, TX",
-    "headshot": "gallery/outdoor-lighting.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "outdoor-lighting.md",
-      "fileName": "outdoor-lighting.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "outdoor-lighting"
+    name: 'Outdoor permanent lighting',
+    location: 'Little Elm, TX',
+    headshot: 'gallery/outdoor-lighting.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'outdoor-lighting.md',
+      fileName: 'outdoor-lighting.md',
+      directory: '.',
+      extension: 'md',
+      path: 'outdoor-lighting',
     },
-    "slug": "outdoor-permanent-lighting"
+    slug: 'outdoor-permanent-lighting',
   },
   {
-    "name": "Outdoor tile instalation",
-    "location": "Little Elm, TX",
-    "headshot": "gallery/outdoor-tile.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "outdoor-tile.md",
-      "fileName": "outdoor-tile.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "outdoor-tile"
+    name: 'Outdoor tile instalation',
+    location: 'Little Elm, TX',
+    headshot: 'gallery/outdoor-tile.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'outdoor-tile.md',
+      fileName: 'outdoor-tile.md',
+      directory: '.',
+      extension: 'md',
+      path: 'outdoor-tile',
     },
-    "slug": "outdoor-tile-instalation"
+    slug: 'outdoor-tile-instalation',
   },
   {
-    "name": "Pax closet installation",
-    "location": "Little Elm, TX",
-    "headshot": "gallery/pax-closet-installation.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "pax-closet-installation.md",
-      "fileName": "pax-closet-installation.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "pax-closet-installation"
+    name: 'Pax closet installation',
+    location: 'Little Elm, TX',
+    headshot: 'gallery/pax-closet-installation.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'pax-closet-installation.md',
+      fileName: 'pax-closet-installation.md',
+      directory: '.',
+      extension: 'md',
+      path: 'pax-closet-installation',
     },
-    "slug": "pax-closet-installation"
+    slug: 'pax-closet-installation',
   },
   {
-    "name": "Restroom remodeling",
-    "location": "Aubrey, TX",
-    "headshot": "gallery/restroom-remodeling.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "restroom-remodeling.md",
-      "fileName": "restroom-remodeling.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "restroom-remodeling"
+    name: 'Restroom remodeling',
+    location: 'Aubrey, TX',
+    headshot: 'gallery/restroom-remodeling.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'restroom-remodeling.md',
+      fileName: 'restroom-remodeling.md',
+      directory: '.',
+      extension: 'md',
+      path: 'restroom-remodeling',
     },
-    "slug": "restroom-remodeling"
+    slug: 'restroom-remodeling',
   },
   {
-    "name": "Shower tile work",
-    "location": "Aubrey, TX",
-    "headshot": "gallery/tile-work.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "tile-work.md",
-      "fileName": "tile-work.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "tile-work"
+    name: 'Shower tile work',
+    location: 'Aubrey, TX',
+    headshot: 'gallery/tile-work.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'tile-work.md',
+      fileName: 'tile-work.md',
+      directory: '.',
+      extension: 'md',
+      path: 'tile-work',
     },
-    "slug": "shower-tile-work"
+    slug: 'shower-tile-work',
   },
   {
-    "name": "Wood wall stain",
-    "location": "Little Elm, TX",
-    "headshot": "gallery/wall-wood-stain.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "wall-wood-stain.md",
-      "fileName": "wall-wood-stain.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "wall-wood-stain"
+    name: 'Wood wall stain',
+    location: 'Little Elm, TX',
+    headshot: 'gallery/wall-wood-stain.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'wall-wood-stain.md',
+      fileName: 'wall-wood-stain.md',
+      directory: '.',
+      extension: 'md',
+      path: 'wall-wood-stain',
     },
-    "slug": "wood-wall-stain"
+    slug: 'wood-wall-stain',
   },
   {
-    "name": "Wood accent wall",
-    "location": "Frisco, TX",
-    "headshot": "gallery/wood-accent-wall.jpg",
-    "content": "Coming soon.",
-    "_meta": {
-      "filePath": "wood-accent-wall.md",
-      "fileName": "wood-accent-wall.md",
-      "directory": ".",
-      "extension": "md",
-      "path": "wood-accent-wall"
+    name: 'Wood accent wall',
+    location: 'Frisco, TX',
+    headshot: 'gallery/wood-accent-wall.jpg',
+    content: 'Coming soon.',
+    _meta: {
+      filePath: 'wood-accent-wall.md',
+      fileName: 'wood-accent-wall.md',
+      directory: '.',
+      extension: 'md',
+      path: 'wood-accent-wall',
     },
-    "slug": "wood-accent-wall"
-  }
+    slug: 'wood-accent-wall',
+  },
 ]
